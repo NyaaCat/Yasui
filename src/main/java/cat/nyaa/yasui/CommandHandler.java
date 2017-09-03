3827a6b3c5261d04c90a2bfceef28980fce14f31
cyilin <cyilin@users.noreply.github.com>
2017-09-03 21:19:25 +0800

    update to NyaaCore 4.0

--- src/main/java/cat/nyaa/yasui/CommandHandler.java
+++ src/main/java/cat/nyaa/yasui/CommandHandler.java
@@ -6,7 +6,7 @@ import cat.nyaa.nyaacore.LanguageRepository;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 
-public class CommandHandler extends CommandReceiver<Main> {
+public class CommandHandler extends CommandReceiver {
     private final Main plugin;
 
     public CommandHandler(Main plugin, LanguageRepository i18n) {
